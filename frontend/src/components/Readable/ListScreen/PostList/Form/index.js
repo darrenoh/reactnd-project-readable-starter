f612648214decef068082b6adcb2b4e9b6ca4436
Darren Oh <darren@oh.name>
2018-03-18 03:13:50 -0400

    feat: Remove unused directories

--- frontend/src/components/Readable/ListScreen/PostList/Form/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * @file
- * Add or edit form for post list component.
- *
- * Requirements
- * - Existing post values should be populated.
- * - New post category should be populated if post list is filtered.
- */
